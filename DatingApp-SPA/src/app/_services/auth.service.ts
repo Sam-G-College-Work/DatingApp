1cb13f103f3d4659348aa3d771aa25fb56f4d44a
Sam Gallagher <sam@Sams-MacBook-Pro-2.local>
2020-02-12 18:26:58 +0000

    ErrorWhenLoggingOut

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({ // allows us to inject things into our service
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
 constructor(private http: HttpClient) { }
 
 login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
+  return this.http
+  .post(this.baseUrl + 'login', model,)
   .pipe(
     map((response: any) => {
       const user = response;
@@ -24,6 +27,11 @@ login(model: any) {
 
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
